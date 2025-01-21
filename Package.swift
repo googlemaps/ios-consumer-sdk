4449972d4207f0ef99a60606b1664710a759dc93
Zion Calvo <zioncalvo@google.com>
2025-01-21 10:43:13 -0800

    feat: 9.2.0

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      https://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -25,8 +25,8 @@ let package = Package(
     .binaryTarget(
       name: "GoogleRidesharingConsumer",
       url:
-        "https://dl.google.com/geosdk/swiftpm/9.0.0/GoogleRidesharingConsumer_3p.xcframework.zip",
-      checksum: "5e9ff7d30325ded928a3e9f64266b93021b2206586b5a53477e83c66cbfdf4a1"
+        "https://dl.google.com/geosdk/swiftpm/9.2.0/GoogleRidesharingConsumer_3p.xcframework.zip",
+      checksum: "e51d555b99605dbda6b8932bbe185d164cddfc5877a326dd27b70b78b3943c24"
     ),
     .target(
       name: "GoogleRidesharingConsumerTarget",
